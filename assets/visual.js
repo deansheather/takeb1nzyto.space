b2af8d4571570da10bdb69f5c0ccb82e4a1ca2d0
Caleb <Zeludon@users.noreply.github.com>
2016-07-05 15:21:23 +1000

    Update visual.js

--- assets/visual.js
+++ assets/visual.js
@@ -18,34 +18,34 @@ function visualize(stream) {
 
   function draw() {
     if (isVisual) {
-    drawVisual = requestAnimationFrame(draw);
-    analyser.getByteTimeDomainData(dataArray);
-
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#bdbdbd";
-
-    ctx.beginPath();
-
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
-
-    for(var i = 0; i < bufferLength; i++) {
-      var v = dataArray[i] / 128.0;
-      var y = v * HEIGHT/2;
-
-      if(i === 0) {
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
-      }
-      x += sliceWidth;
-    }
-    ctx.lineTo(canvas.width, canvas.height / 2);
-    ctx.stroke();
-    draw();
+	    drawVisual = requestAnimationFrame(draw);
+	    analyser.getByteTimeDomainData(dataArray);
+	
+	    ctx.lineWidth = 2;
+	    ctx.strokeStyle = "#bdbdbd";
+	
+	    ctx.beginPath();
+	
+	    var sliceWidth = WIDTH * 1.0 / bufferLength;
+	    var x = 0;
+	
+	    for(var i = 0; i < bufferLength; i++) {
+	      	var v = dataArray[i] / 128.0;
+	      	var y = v * HEIGHT/2;
+	
+	      	if(i === 0) {
+	        	ctx.moveTo(x, y);
+	      	} else {
+	        	ctx.lineTo(x, y);
+	      	}
+	      	x += sliceWidth;
+	    }
+	    ctx.lineTo(canvas.width, canvas.height / 2);
+	    ctx.stroke();
   } else {
     ctx.clearRect(0, 0, WIDTH, HIEGHT);
   }
+ }
+ draw();
 }
-
 window.onload = visualize(audio);
