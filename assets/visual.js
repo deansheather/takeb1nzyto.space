a3e0368719bdfe3ee11e8e5cfad58d5a24cef4f4
Caleb <Zeludon@users.noreply.github.com>
2016-07-05 14:34:43 +1000

    Added Visualizer

---
+++ assets/visual.js
@@ -0,0 +1,43 @@
+function visualize(stream) {
+  var context = new AudioContext();
+  var analyser = context.createAnalyser();
+  var source = context.createMediaElementSource(audio);
+  source.connect(analyser);
+  analyser.connect(context.destination);
+  
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.frequencyBinCount; 
+  var dataArray = new Uint8Array(bufferLength); 
+
+  function draw() {
+    drawVisual = requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#bdbdbd";
+
+    ctx.beginPath();
+
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+
+    for(var i = 0; i < bufferLength; i++) {
+      var v = dataArray[i] / 128.0;
+      var y = v * HEIGHT/2;
+
+      if(i === 0) {
+        ctx.moveTo(x, y);
+      } else {
+        ctx.lineTo(x, y);
+      }
+      x += sliceWidth;
+    }
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.stroke();
+  }
+  draw();
+}
