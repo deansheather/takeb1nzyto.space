2f69a1de57e5999386a6498eb82fc5b09b5137c5
Caleb <Zeludon@users.noreply.github.com>
2016-07-05 14:47:25 +1000

    Update visual.js

--- assets/visual.js
+++ assets/visual.js
@@ -5,14 +5,14 @@ function visualize(stream) {
   source.connect(analyser);
   analyser.connect(context.destination);
   
-  
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
 
   analyser.fftSize = 2048;
   var bufferLength = analyser.frequencyBinCount; 
   var dataArray = new Uint8Array(bufferLength); 
 
   function draw() {
-    var d = getDimensions();
     drawVisual = requestAnimationFrame(draw);
 
     analyser.getByteTimeDomainData(dataArray);
@@ -22,12 +22,12 @@ function visualize(stream) {
 
     ctx.beginPath();
 
-    var sliceWidth = d.w * 1.0 / bufferLength;
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
     var x = 0;
 
     for(var i = 0; i < bufferLength; i++) {
       var v = dataArray[i] / 128.0;
-      var y = v * d.y/2;
+      var y = v * HEIGHT/2;
 
       if(i === 0) {
         ctx.moveTo(x, y);
@@ -36,7 +36,7 @@ function visualize(stream) {
       }
       x += sliceWidth;
     }
-    ctx.lineTo(d.w, d.h / 2);
+    ctx.lineTo(canvas.width, canvas.height / 2);
     ctx.stroke();
   }
   draw();
